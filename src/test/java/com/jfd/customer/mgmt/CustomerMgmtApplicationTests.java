02a7d36246d94e2980ee9fdc3599ae5c32077425
application-development <com.pratap@gmail.com>
2025-05-28 06:10:20 +0530

    service created

---
+++ src/test/java/com/jfd/customer/mgmt/CustomerMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfd.customer.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
